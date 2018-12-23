9e38962c5685c4d21a54211fb638956c8b695d4d
Ashmita Kapoor <ashmita.kapoor@yahoo.in>
2018-12-23 15:27:12 +0530

    Removing Netflix libraries

--- src/main/java/com/redhat/ssi/currency/CurrencyApplication.java
+++ src/main/java/com/redhat/ssi/currency/CurrencyApplication.java
@@ -3,13 +3,9 @@ package com.redhat.ssi.currency;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@RefreshScope
 public class CurrencyApplication {
 
     public static void main(String[] args) {
